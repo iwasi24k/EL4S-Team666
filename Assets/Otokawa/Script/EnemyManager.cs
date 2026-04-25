999c498f43399c74d2d10cdaf8b57ef31a8f3f3f
YuukiOtokawa <ths30871@ths.hal.ac.jp>
2026-04-25 14:42:58 +0900

    EnemyManagerの作成
    feat : エネミーの統一パラメータを格納するEnemyManagerクラスを作成
    feat : EnemyManagerのプレファブを作成
    feat : EnemyControlに画面に入った時点でのプレイヤーがいる方向に移動を始める処理を追加

---
+++ Assets/Otokawa/Script/EnemyManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    public static float _MoveSpeed   { set; get; }
+    public static float _DamageScale { set; get; }
+    public static float _BulletSize  { set; get; }
+    public static float _BulletInterval { set; get; }
+    public static float _HitBoxSize  { set; get; }
+    public static float _Alpha       { set; get; }
+}
